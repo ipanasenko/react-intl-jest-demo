3962bb5f34a338636d826e5a53c0e7fa8ea113e0
Ilya Panasenko <hottdogg2008@gmail.com>
2019-07-15 12:16:56 +0300

    another commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { FormattedMessage } from "react-intl";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <FormattedMessage id="key" />
         </p>
         <a
           className="App-link"
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <FormattedMessage id="key2" />
         </a>
       </header>
     </div>
---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { IntlProvider } from "react-intl";
+
+ReactDOM.render(
+  <IntlProvider
+    locale="en"
+    messages={{ key: "Edit App.js and save to reload.", key2: "Learn React!!!11" }}
+    textComponent={React.Fragment}
+  >
+    <App />
+  </IntlProvider>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
